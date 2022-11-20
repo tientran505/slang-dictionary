536dc9d1be9b505484dcefe84152619969b02be3
Tien <73010955+tientran505@users.noreply.github.com>
2022-11-20 21:37:00 +0700

    fea: search by slang word

--- src/SlangDictionary.java
+++ src/SlangDictionary.java
@@ -1,22 +1,31 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.sql.Array;
 import java.util.*;
 
 /**
- * PACKAGE_NAME
- * Created by PC
+ * Created by Thien Tien
  * Date 11/20/2022 - 3:18 PM
- * Description: ...
+ * Description: Implement features of Slang Dictionary Application
  */
 public class SlangDictionary {
+    /**
+     * Attribute of SlangDictionary: TreeMap with key is String and value is the list of String because
+     * a slang word can have many meanings
+     */
     private TreeMap<String, List<String>> slangDictionary;
 
+    /**
+     * Constructor of SlangDictionary
+     */
     public SlangDictionary() {
         slangDictionary = new TreeMap<>();
     }
 
+    /**
+     * Import a list of slang words and meanings to memory
+     * @param fileName name of file that stores slang word and its meaning(s)
+     */
     public void importDictionary(String fileName) {
         if (!slangDictionary.isEmpty()) {
             slangDictionary.clear();
@@ -51,19 +60,37 @@ public class SlangDictionary {
             }
 
             reader.close();
-
-            for(Map.Entry m: slangDictionary.entrySet()) {
-                System.out.println(m.getKey() + ": " + m.getValue());
-            }
-
         } catch (IOException err) {
             err.printStackTrace();
         }
     }
 
+    /**
+     * Search by Slang Word
+     * @param slang slang word to look for
+     * @return list of meanings of slang word or null if the slang is not found
+     */
+    public List<String> searchBySlangWord(String slang) {
+        if (slangDictionary.containsKey(slang)) {
+            return slangDictionary.get(slang);
+        }
+        return null;
+    }
+
+
+
     public static void main(String[] args) {
         SlangDictionary app = new SlangDictionary();
         app.importDictionary("slang.txt");
+
+        List<String> def = app.searchBySlangWord(">:O");
+        if (def != null) {
+            for (String str: def) {
+                System.out.println(str);
+            }
+        } else {
+            System.out.println("Inexisted slang word");
+        }
     }
 
 }
